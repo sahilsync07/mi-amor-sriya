19c5af8747ba41edd048681a327028c197d7b2a8
Sahil kumar <sahilsync07@gmail.com>
2026-02-13 02:14:59 +0530

    chore: update hero text to reflect lifelong friendship

--- src/components/HeroNew.jsx
+++ src/components/HeroNew.jsx
@@ -75,7 +75,7 @@ export default function HeroNew({ girlfriendName = "Sriya" }) {
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ delay: 1.5, duration: 1 }}
                     >
-                        Welcome to our world
+                        A Lifetime of Us
                     </motion.p>
 
                     {/* Main Name - Elegant Reveal */}
@@ -103,7 +103,7 @@ export default function HeroNew({ girlfriendName = "Sriya" }) {
                         animate={{ opacity: 0.8 }}
                         transition={{ delay: 3, duration: 1 }}
                     >
-                        Since January 9, 2019
+                        Forever begins in LKG
                     </motion.p>
                 </div>
 
